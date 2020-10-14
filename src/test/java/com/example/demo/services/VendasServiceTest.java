9f1fdabf0e3d917ef6b80ea9d73ccd881a8a4de6
gphelipe1 <gpcfreitas@gmail.com>
2020-10-14 18:39:06 -0300

    Deletando desnecessário para o código

--- src/test/java/com/example/demo/services/VendasServiceTest.java
+++ src/test/java/com/example/demo/services/VendasServiceTest.java
@@ -31,10 +31,18 @@ public class VendasServiceTest {
         LocalDate date = LocalDate.now();
 
         Vendas venda1 = Vendas.builder().data_venda(date).build();
-        Vendas vendSalv1 = repository.save(venda1);
+        Vendas vendaSalv1 = repository.save(venda1);
+
+        Assertions.assertNotNull(vendaSalv1);
+        Assertions.assertEquals(venda1.getNumeroVenda(), vendaSalv1.getNumeroVenda());
+        Assertions.assertEquals(venda1.getData_venda(), vendaSalv1.getData_venda());
 
         Vendas venda2 = Vendas.builder().data_venda(date).build();
         Vendas vendaSalv2 = repository.save(venda2);
+
+        Assertions.assertNotNull(vendaSalv2);
+        Assertions.assertEquals(venda2.getNumeroVenda(), vendaSalv2.getNumeroVenda());
+        Assertions.assertEquals(venda2.getData_venda(), vendaSalv2.getData_venda());
 
         List<Vendas> listaVendas = service.listarVendas();
 
